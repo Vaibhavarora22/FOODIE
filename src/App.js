7896e1d1803d3e849a6e315722d3fc84c6eb1045
Vaibhav Arora <118902823+Vaibhavarora22@users.noreply.github.com>
2023-08-24 14:53:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Work from "./Components/Work";
+import Testimonial from "./Components/Testimonial";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+      {Home}
+      <About />
+      {About}
+      <Work />
+      {Work}
+      <Testimonial />
+      {Testimonial}
+      <Contact />
+      {Contact}
+      <Footer />
+      {Footer}
+    </div>
+  );
+}
